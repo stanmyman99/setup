c594c18590e063434db49eafbc1421011e2984f8
Stan Perry <stanmyman99@protonmail.com>
2024-05-25 10:40:03 -0600

    asdf

--- replace-colon.py
+++ replace-colon.py
@@ -3,16 +3,41 @@
 
 import os
 
-def list_files(directory):
+def list_files(directory, chars):
+    files = []
     for item in os.listdir(directory):
-        item_path = os.path.join(directory, item)
-        if os.path.isfile(item_path):
-            print(f"File: {item}")
-        elif os.path.isdir(item_path):
-            print(f"Directory: {item}")
+        if item.find(chars) >= 0:
+            if os.path.isfile(item):
+                files.append(item)
+                #print(f"File: {item}")
+            elif os.path.isdir(item):
+                print(f"Skipped Directory: {item}")
+    return files
+
+def rename_files(files, badchars, newchars):
+    for file in files:
+        new_file = file.replace(badchars, newchars)
+        print(f"{file} => {new_file}")
+        os.rename(file, new_file)
+
 
 # specify the directory you want to list files from
 directory = '.'
 
-list_files(directory)
+#files = list_files(directory, ":")
+#rename_files(files, ":", " -- ")
 
+#files = list_files(directory, " -- ")
+#rename_files(files, " -- ", ",")
+
+#files = list_files(directory, "**")
+#rename_files(files, "**", "")
+
+#files = list_files(directory, '"')
+#rename_files(files, '"', "")
+
+#files = list_files(directory, ',')
+#rename_files(files, ',', "")
+
+files = list_files(directory, '?')
+rename_files(files, '?', "")
