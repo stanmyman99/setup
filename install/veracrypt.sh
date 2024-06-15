01a9799398d867483f026ce0e3af34ba0f2f8eeb
stan <stan@myman99>
2024-06-15 13:36:18 -0600

    veracrypt installation

--- install/veracrypt.sh
+++ install/veracrypt.sh
@@ -3,8 +3,8 @@
 pushd .
 mkdir ~/Apps/veracrypt
 cd ~/Apps/veracrypt
-wget https://www.veracrypt.fr/code/VeraCrypt/snapshot/VeraCrypt_1.26.7.tar.gz
 sudo apt install g++ libfuse-dev pkg-config yasm libwxbase3.0-dev libpcsclite-dev
+wget https://www.veracrypt.fr/code/VeraCrypt/snapshot/VeraCrypt_1.26.7.tar.gz
 tar -xvf VeraCrypt_1.26.7.tar.gz
 cd VeraCrypt_1.26.7/src
 make NOGUI=1
