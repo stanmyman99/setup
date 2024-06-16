34cc0fb3ce507ddab6d5881b358b6376aae4800a
stan <stan@myman99.com>
2024-06-16 08:27:29 -0600

    app frameworks

---
+++ install/appframeworks.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "# ------------------------------------------------------------------------"
+echo "# App frameworks (snap, flatpak) installation"
+echo "# ------------------------------------------------------------------------"
+
+
+if command -v snap > /dev/null
+then
+    echo "Snap is already installed"
+else
+    sudo apt install snapd -y    
+fi
+
+if command -v flatpak > /dev/null
+then
+    echo "Flatpak is already installed"
+else
+    sudo apt install flatpak -y
+    sudo apt install gnome-software-plugin-flatpak -y
+    sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+fi
+
+echo "Completed app frameworks installation"
+echo " "
+
+
--- provision.sh
+++ provision.sh
@@ -5,8 +5,10 @@ echo "# New device provisioing script"
 echo "# ------------------------------------------------------------------------"
 echo " "
 
-bash install/bash.sh
-bash install/git.sh
+bash install/appframeworks.sh
 bash install/softlinks.sh
 bash install/veracrypt.sh
+bash install/gnome.sh
+bash install/bash.sh
+bash install/git.sh
 
