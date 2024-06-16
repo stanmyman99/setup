cefb1b11f8a01327ba59c09d2d4ab3713991b5dc
stan <stan@myman99.com>
2024-06-16 11:14:42 -0600

    Cleanup

--- install/helper-apt.sh
+++ install/helper-apt.sh
@@ -1,7 +1,15 @@
+# Usage
+#  $1 = the command that launches the app
+#  $2 = the name of the package, sudo apt install <package>
+#  $3 = a friendly name
+#
+#  Most of the time, 1 and 2 are the same.
+
+
 if command -v "$1" > /dev/null 2> /dev/null
 then
     echo "$2 is already installed"
 else
     echo "Installing $2..."
-    sudo apt install "$1" -y >> ~/.provision.log 2>> ~/.provision.log
+    sudo apt install "$3" -y >> ~/.provision.log 2>> ~/.provision.log
 fi
--- install/mullvad.sh
+++ install/mullvad.sh
@@ -11,8 +11,8 @@ else
     echo "Installing Mullvad VPN..."
     sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
     echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
-    sudo apt update
-    sudo apt install mullvad-vpn -y >> ~/.provision.log
+    sudo apt update >> ~/.provision.log 2>> ~/.provision.log
+    bash install/helper-apt.sh mullvad mullvad-vpn "Mullvad VPN"
 fi
 
 
