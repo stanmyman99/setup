4a9b57f757e7f5cde505b2a0a268b8ba68be7aad
stan <stan@myman99.com>
2024-06-22 18:57:18 -0600

    Only Office and gnome favorites cleanup

--- install/apps.sh
+++ install/apps.sh
@@ -12,11 +12,15 @@ else
     mkdir ~/Apps
 fi
 
+# Remove any unwanted apps that are installed by default
+#
+echo "Removing unwanted apps..."
+echo "Removing Libre Office..."
+sudo apt-get remove --purge "libreoffice*" -y >> ~/.provision.log 2>> ~/.provision.log
+
+echo "Installing desired apps..."
 bash install/helper-apt.sh keepassxc keepassxc "KeePassXC"
 bash install/helper-apt.sh rpi-imager rpi-imager "RPI Imager"
-
-sudo apt install multitail tree zoxide trash-cli fzf bash-completion -y >> ~/.provision.log 2>> ~/.provision.log
-bash install/helper-apt.sh android-tools-adb "Android Debugging Tools"
 
 #bash install/helper-flatpak.sh com.nextcloud.desktopclient.nextcloud "NextCloud Desktop Client"
 bash install/helper-flatpak.sh com.brave.Browser "Brave Browser"
@@ -25,6 +29,10 @@ bash install/helper-flatpak.sh org.chromium.Chromium "Chromium Browser"
 bash install/helper-flatpak.sh org.freefilesync.FreeFileSync "FreeFileSync"
 bash install/helper-flatpak.sh ch.protonmail.protonmail-bridge  "Proton Mail Bridge"
 bash install/helper-flatpak.sh com.github.micahflee.torbrowser-launcher "Tor Browser"
+bash install/helper-flatpak.sh org.onlyoffice.desktopeditors "Only Office Desktop"
+
+sudo apt install multitail tree zoxide trash-cli fzf bash-completion -y >> ~/.provision.log 2>> ~/.provision.log
+bash install/helper-apt.sh android-tools-adb "Android Debugging Tools"
 
 echo "Completed app installation "
 echo " "
--- install/helper-flatpak.sh
+++ install/helper-flatpak.sh
@@ -1,4 +1,10 @@
 
+# Usage
+#  $1 = the command that launches the app (eg org.onlyoffice.desktopeditors)
+#  $2 = a friendly name (eg "Only Office Desktop")
+#
+#  Most of the time, 1 and 2 are the same.
+
 flatpak info "$1" > /dev/null 2>> /dev/null
 if [ $? -eq 0 ]
 then 
