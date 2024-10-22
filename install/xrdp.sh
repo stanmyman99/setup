82fde2824771828f40066f9fa0bf5ebbfe808144
stan <stan@myman99.com>
2024-10-21 20:31:53 -0600

    rdp

--- install/xrdp.sh
+++ install/xrdp.sh
@@ -4,7 +4,7 @@ echo "------------------------------------------------------------------------"
 echo " XRDP Server"
 echo "------------------------------------------------------------------------"
 
-sudo apt install xrdp
+sudo apt install xrdp -y
 echo "gnome-session" > ~/.xsession
 sudo systemctl enable xrdp
 sudo systemctl start xrdp
